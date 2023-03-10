79624052de30309203ebb6e0003c714150481b36
ConstantineM <mez365@mail.ru>
2023-03-10 22:44:53 +0300

    9. ThreadLocal demo.

---
+++ src/main/java/ru/job4j/basic/threadlocal/FirstThread.java
@@ -0,0 +1,18 @@
+package ru.job4j.basic.threadlocal;
+
+/**
+ * Для демонстрации возможностей
+ * {@link ThreadLocal} мы создали
+ * данный класс и поместили его
+ * в переменную {@link ThreadLocal}.
+ * Данные этого класса не будут
+ * видны в других нитях.
+ */
+public class FirstThread extends Thread {
+
+    @Override
+    public void run() {
+        ThreadLocalDemo.tl.set("This is thread #1..");
+        System.out.println(ThreadLocalDemo.tl.get());
+    }
+}
