c8d16c87a42072a3d9fad97aabfe8e60a479eae7
ConstantineM <mez365@mail.ru>
2023-03-30 15:53:20 +0300

    5. Состояние гонки

---
+++ src/main/java/ru/job4j/basic/sync/sharedresources/racecondition/RaceConditionExample.java
@@ -0,0 +1,31 @@
+package ru.job4j.basic.sync.sharedresources.racecondition;
+
+/**
+ * Данный класс описывает пример
+ * состояния гонки.
+ *
+ * <p>Состояние гонки наступает,
+ * когда две и более нитей имеют
+ * одновременный доступ к общему
+ * ресурсу, вследствие чего
+ * приложение имеет нестабильное
+ * поведение.
+ */
+public class RaceConditionExample {
+
+    public static int num = 0;
+
+    public static void main(String[] args) {
+        Runnable task = () -> {
+            for (int i = 0; i < 99999; i++) {
+                int current = num;
+                int next = ++num;
+                if (current + 1 != next) {
+                    throw new IllegalStateException("Некорректное сравнение: " + current + " + 1 = " + next);
+                }
+            }
+        };
+        new Thread(task).start();
+        new Thread(task).start();
+    }
+}
