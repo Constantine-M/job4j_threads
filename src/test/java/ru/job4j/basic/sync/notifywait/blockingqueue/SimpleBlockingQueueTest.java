040c7148ae3f934eeae87c773647f66d7fd79d88
ConstantineM <mez365@mail.ru>
2023-04-04 12:38:24 +0300

    3. Junit тест для блокирующей очереди. [#68589]

--- src/test/java/ru/job4j/basic/sync/notifywait/blockingqueue/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/basic/sync/notifywait/blockingqueue/SimpleBlockingQueueTest.java
@@ -2,40 +2,63 @@ package ru.job4j.basic.sync.notifywait.blockingqueue;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class SimpleBlockingQueueTest {
 
+    /**
+     * В данном тесте переменная buffer нужна,
+     * чтобы собрать данные в список и
+     * проверить их в конце.
+     *
+     * <p>Обрати внимание, у потребителя
+     * вначале двойная проверка. Это нужно,
+     * чтобы проверить, что список пустой,
+     * т.к. без этой проверки есть шанс того,
+     * чтобы мы сможем прочитать НЕ ВСЕ данные.
+     *
+     * <p>Тест будет неполным, если не
+     * дописать {@link Thread#join()}.
+     * Главная нить может не дождаться выполнения
+     * потребителя и производителя.
+     * И, соответственно, тест завалится.
+     */
     @Test
-    public void whenQueueFullCapacity() throws InterruptedException {
-        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(3);
+    public void whenBlockingQueueWorksCorrectly() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
         Thread producer = new Thread(
                 () -> {
-                    for (int i = 0; i < 3; i++) {
-                        try {
+                    try {
+                        for (int i = 0; i < 5; i++) {
                             queue.offer(i);
-                        } catch (InterruptedException e) {
-                            Thread.currentThread().interrupt();
                         }
-                    }
-                }
-        );
-        Thread consumer = new Thread(
-                () -> {
-                    for (int i = 0; i < 3; i++) {
-                        try {
-                            queue.poll();
-                        } catch (InterruptedException e) {
-                            Thread.currentThread().interrupt();
-                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        Thread.currentThread().interrupt();
                     }
                 }
         );
         producer.start();
-        producer.join();
-        assertThat(queue.size()).isEqualTo(3);
+        Thread consumer = new Thread(
+                () -> {
+                    while (queue.size() != 0 || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
         consumer.start();
+        producer.join();
+        consumer.interrupt();
         consumer.join();
-        assertThat(queue.size()).isEqualTo(0);
+        assertThat(buffer).isEqualTo(Arrays.asList(0, 1, 2, 3, 4));
     }
 }
