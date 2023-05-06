f0caad28404239dc778e0837a04c24e083ffa61e
ConstantineM <mez365@mail.ru>
2023-05-06 10:59:04 +0300

    3. ForkJoinPool [#315067] - fix #3

--- src/main/java/ru/job4j/multithreading/pools/forkjoinpool/ParallelSearchIndex.java
+++ src/main/java/ru/job4j/multithreading/pools/forkjoinpool/ParallelSearchIndex.java
@@ -74,10 +74,10 @@ public class ParallelSearchIndex<T> extends RecursiveTask<Integer> {
      */
     @Override
     protected Integer compute() {
-        if ((from - to) <= 10) {
+        if ((to - from) <= 10) {
             return linearSearch(array, element);
         }
-        int mid = ((from - to)) / 2;
+        int mid = ((to - from)) / 2;
         ParallelSearchIndex<T> ps1 = new ParallelSearchIndex<>(array, element, from, mid);
         ParallelSearchIndex<T> ps2 = new ParallelSearchIndex<>(array, element, mid + 1, to);
         ForkJoinTask.invokeAll(ps1, ps2);
